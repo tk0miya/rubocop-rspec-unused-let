3dd5f6754fe1c465195a29bf84d91ab1d5aed593
Takeshi KOMIYA <i.tkomiya@gmail.com>
2026-07-13 19:18:58 +0900

    Initial commit (Just after "bundle gem")

---
+++ lib/rubocop/rspec/unused/let.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "let/version"
+
+module Rubocop
+  module Rspec
+    module Unused
+      module Let
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/rubocop/rspec/unused/let/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Rubocop
+  module Rspec
+    module Unused
+      module Let
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
